5669a9ddfab225c78267ceac54bb6f6ba6730c0c
balavignesh6779 <balavigneshp06@gmail.com>
2026-02-21 10:25:12 +0530

    message

---
+++ OOPSBannerApp.java
@@ -0,0 +1,38 @@
+/**
+ * OOPSBannerApp
+ *
+ * This class prints the word "OOPS" in banner format
+ * using asterisks (*) and spaces.
+ * Each character is 7 lines high and 9 characters wide.
+ */
+public class OOPSBannerApp {
+
+    /**
+     * The main method is the entry point of the application.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println("  *****  " + "  *****  " + " ********" + "  ****** ");
+
+        // Line 2
+        System.out.println(" *     * " + " *     * " + " *      *" + " *       ");
+
+        // Line 3
+        System.out.println("*       *" + "*       *" + " *      *" + " *       ");
+
+        // Line 4
+        System.out.println("*       *" + "*       *" + " ********" + "  *****  ");
+
+        // Line 5
+        System.out.println("*       *" + "*       *" + " *       " + "       * ");
+
+        // Line 6
+        System.out.println(" *     * " + " *     * " + " *       " + " *     * ");
+
+        // Line 7
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
+    }
+}
